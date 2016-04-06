676861f76542421a9e0784226757746235164e97
qinhan <1036474541@qq.com>
2016-04-06 17:48:41 +0800

    1

---
+++ text_test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qinhan
+ * Date: 2016/4/6
+ * Time: 17:38
+ */
+use Workerman\Worker;
+require './Workerman-master/Autoloader.php';
+$global_uid = 0;
+function handle_connect($connect){
+	global $worker,$global_uid;
+	$connect->uid = $global_uid;
+}
+function handle_mes($connect,$data){
+	global $worker;
+	foreach ($worker as $con){
+		$con->send("user$connect->uid said:$data");
+	}
+
+}
+function handle_close($connect){
+	global $worker;
+	foreach ($worker as $con){
+		$con->send("user$connect->uid said:close");
+	}
+
+}
+$woker = new Worker("text://0.0.0.0:2347");
+$woker->count = 1;
+$woker->onConnect = 'handle_connect';
+$woker->onMessage = 'handle_mes';
+$woker->onClose = 'handle_close';
+Worker::runAll();
